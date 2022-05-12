701ca747c9ecb3df2827e6a9f36bd46b57315a0a
Joel Nyaga <joelnyagamuriithi@gmail.com>
2022-05-13 01:06:59 +0300

    Changing the print struct file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,28 +1,28 @@
 #include "dog.h"
-
+#include <stddef.h>
+#include <stdio.h>
 /**
-* print_dog - prints a struct dog
-*@d: dog identification
-*
-*/
+  *print_dog - a function that print a Structure
+  *@d : our Structure
+  */
 
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-return; 
-
-if ((*d).name == NULL)
-printf("Name: (nil)\n");
-else
+if (d != NULL)
+{
+if (d->name != NULL)
+{
 printf("Name: %s\n", d->name);
-
-if ((*d).age < 0)
-printf("Age: (nil)\n");
+}
 else
-printf("Age: %f\n", d->age);
-
-if ((*d).owner == NULL)
-printf("Owner: (nil)\n");
-else
+printf("Name: nil\n");
+printf("Age: %f\n", (*d).age);
+if (d->owner != NULL)
+{
 printf("Owner: %s\n", d->owner);
 }
+else
+printf("Owner: nil\n");
+}
+}
+
