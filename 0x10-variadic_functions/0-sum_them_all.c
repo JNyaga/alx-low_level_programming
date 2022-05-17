fd3893e1deb2a7ed985c3596d700ae973962608d
Joel Nyaga <joelnyagamuriithi@gmail.com>
2022-05-17 20:48:34 +0300

    Commiting the variadic function files

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,33 +1,26 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-
 /**
-*sum_them_all - sums all its parameters
-*@n: first parameter
-*
-*Return: 0 if n is null
-* or sum of parameters in other case
-*/
-
+ *sum_them_all - returns a sum of all its parameters
+ *
+ *@n:no. of arguements
+ *
+ *Return:sum
+ *
+ */
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int i;
-va_list list;
-double sum = 0;
+	va_list va;
+	int sum = 0;
+	unsigned int i;
 
-if (n == 0)
-{
-return (0);
+	if (n == 0)
+		return (0);
+	va_start(va, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(va, int);
+	}
+	va_end(va);
+	return (sum);
 }
-
-va_start(list, n);
-
-for (i = 0; i < n; i++)
-{
-sum += va_arg(list, int);
-}
-
-va_end(list);
-return (sum);
-}
-
