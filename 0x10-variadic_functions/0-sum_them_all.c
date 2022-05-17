7c67f9a87f1dd0523791bcf8f1f3e0fdab6f1e96
Joel Nyaga <joelnyagamuriithi@gmail.com>
2022-05-17 20:19:24 +0300

    Commiting the variadic function files

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,20 +11,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-
-va_list li;
-int sum = 0;
 unsigned int i;
+va_list list;
+double sum = 0;
 
-va_start(li, n);
-
-if (n != 0)
-{
-for (i = 0; i < n; i++)
-sum += va_arg(li, int);
-}
-
-va_end(li);
-return (sum);
-
+	if (n == 0)
+	{
+		return (0);
+	}
+va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);
 }
