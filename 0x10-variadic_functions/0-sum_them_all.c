956d2a877236dfac7a3c7c6fe5b5349a50c4bb90
JNyaga <joelnyagamuriithi@gmail.com>
2022-05-17 10:29:14 -0700

    changing sum file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,15 +15,19 @@ unsigned int i;
 va_list list;
 double sum = 0;
 
-	if (n == 0)
-	{
-		return (0);
-	}
-va_start(list, n);
-	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(list, int);
-	}
-	va_end(list);
-	return (sum);
+if (n == 0)
+{
+return (0);
 }
+
+va_start(list, n);
+
+for (i = 0; i < n; i++)
+{
+sum += va_arg(list, int);
+}
+
+va_end(list);
+return (sum);
+}
+
